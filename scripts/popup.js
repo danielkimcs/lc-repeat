4eb8f332d3820a6e629a3e91870608754934ded3
dknj11902 <dankim20@bergen.org>
2021-08-07 17:03:26 -0400

    Added table of problems due now

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,12 +1,62 @@
 const notesInput = document.getElementById("notes");
 let scheduleButtons = document.getElementsByClassName("schedule-btn");
-let formContainer = document.getElementsByClassName("form-container")[0];
-let saveMessage = document.getElementsByClassName("save-message")[0];
+let tableToggleBtn = document.querySelector(".table-toggle");
+let formContainer = document.querySelector(".form-container");
+let saveMessage = document.querySelector(".save-message");
+let tableContainer = document.querySelector(".problems-container");
+let problemsTable = document.querySelector(".problems-container table");
 
 let problemUrl = undefined;
 let questionNumber = undefined;
 let questionTitle = undefined;
 let questionDifficulty = undefined;
+
+let isTableVisible = true;
+
+const tableHeaders = ['#', 'Problem Title', 'Due Date'];
+
+const difficultyColors = {
+    "Easy": "rgba(0,175,155,0.6)",
+    "Medium": "rgba(255,184,0,0.6)",
+    "Hard": "rgba(255,45,85,0.6)"
+}
+
+const hideElement = (element, hide) => {
+    if (hide) element.classList.add("hide");
+    else element.classList.remove("hide");
+}
+
+const formatDate = (dateString) => {
+    let date = new Date(dateString);
+    return ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear();
+}
+
+const displayProblemsTable = (problems) => {
+    problemsTable.innerHTML = "";
+    let header = problemsTable.insertRow(0);
+    tableHeaders.forEach(text => {
+        let th = document.createElement('th');
+        th.innerHTML = text;
+        header.appendChild(th);
+    });
+
+    Object.keys(problems)
+        .sort((p1, p2) => Date.parse(problems[p1].current.scheduledDate) - Date.parse(problems[p2].current.scheduledDate))
+        .forEach((problemNumber, index) => {
+            let problem = problems[problemNumber];
+            let problemRow = problemsTable.insertRow(index + 1);
+            problemRow.insertCell(0).innerHTML = problemNumber;
+            problemRow.insertCell(1).innerHTML = problem.title;
+            problemRow.insertCell(2).innerHTML = formatDate(problem.current.scheduledDate);
+
+            problemRow.addEventListener('click', () => {
+                chrome.tabs.create({ url: problem.link });
+            });
+
+            problemRow.classList.add('problem-row');
+            problemRow.style.backgroundColor = difficultyColors[problem.difficulty];
+        });
+}
 
 const loadQuestionInfo = async (callback) => {
     if (questionNumber !== undefined) {
@@ -26,13 +76,6 @@ const loadQuestionInfo = async (callback) => {
         return;
     });
 }
-
-Array.prototype.forEach.call(scheduleButtons, button => {
-    button.addEventListener("click", async () => {
-        const numDays = parseInt(button.value);
-        scheduleCurrentProblem(numDays);
-    });
-});
 
 const scheduleCurrentProblem = async (numDays) => {
     loadQuestionInfo(() => {
@@ -72,8 +115,9 @@ const scheduleCurrentProblem = async (numDays) => {
             }
 
             chrome.storage.sync.set({ problems: newProblems }, () => {
-                formContainer.classList.add("hide");
-                saveMessage.classList.remove("hide");
+                hideElement(formContainer, true);
+                hideElement(saveMessage, false);
+                displayProblemsTable(newProblems);
             });
         });
     });
@@ -86,3 +130,38 @@ const fetchQuestionInfo = () => {
     const url = window.location.href;
     return { url, num, title, diff };
 }
+
+const addScheduleBtnListeners = () => {
+    Array.prototype.forEach.call(scheduleButtons, button => {
+        button.addEventListener("click", async () => {
+            const numDays = parseInt(button.value);
+            scheduleCurrentProblem(numDays);
+        });
+    });
+}
+
+chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+    if (tab.url.includes("leetcode.com/problems/")) {
+        addScheduleBtnListeners();
+        tableToggleBtn.innerHTML = "+";
+        isTableVisible = false;
+        hideElement(tableContainer, !isTableVisible);
+    } else {
+        hideElement(formContainer, true);
+        hideElement(tableToggleBtn, true);
+    }
+});
+
+chrome.storage.sync.get("problems", ({ problems }) => {
+    displayProblemsTable(problems);
+});
+
+tableToggleBtn.addEventListener("click", () => {
+    isTableVisible = !isTableVisible;
+    hideElement(tableContainer, !isTableVisible);
+    if (isTableVisible) {
+        tableToggleBtn.innerHTML = "&minus;";
+    } else {
+        tableToggleBtn.innerHTML = "+";
+    }
+});
